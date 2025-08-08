6654157de724184790314ee955058459fe5acdaa
athapol_s <athapol_s@modernform.co.th>
2025-08-08 17:11:03 +0700

    Add

--- server.js
+++ server.js
@@ -154,6 +154,7 @@ io.on('connection', (socket) => {
   });
 });
 
-// ===== Start server =====
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;   // << สำคัญสำหรับ Railway
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
